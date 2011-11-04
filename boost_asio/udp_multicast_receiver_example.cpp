bd2b7c881e42d246d4794d63179a0c4fe589ffb9
Jan Blunck <jblunck@infradead.org>
2011-11-04 08:59:39 +0100

    Add command-line options "h", "i" and "p" to upd_multicast_receiver_example
    The application now listens to the following options:
    
    udp_multicast_receiver -i ipaddr -h ipaddr -p port
    
     -i specify interface on which to listen for multicast datagrams
     -h specify multicast group to join
     -p specify port on which to listen for multicast datagrams
    
    Signed-off-by: Jan Blunck <jblunck@infradead.org>

--- boost_asio/udp_multicast_receiver_example.cpp
+++ boost_asio/udp_multicast_receiver_example.cpp
@@ -1,18 +1,45 @@
 #include "udp_multicast_receiver.hpp"
 #include "datagram_receiver_handler.hpp"
+#include <boost/lexical_cast.hpp>
 #include <iostream>
+#include <unistd.h> // for getopt
 
 typedef udp_multicast_receiver<struct nop_handler,
                                struct cerr_status_listener> receiver_t;
 
-int main()
+using namespace boost::asio::ip;
+
+int main(int argc, char *argv[])
 {
+    // parse options
+    address opts_interface_ipaddr(address_v4::any());
+    address opts_ipaddr;
+    unsigned short opts_port(0);
+
+    int opt;
+    while ((opt = getopt(argc, argv, "i:h:p:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'i':
+            opts_interface_ipaddr = address::from_string(optarg);
+            break;
+        case 'h':
+            opts_ipaddr = address::from_string(optarg);
+            break;
+        case 'p':
+            opts_port = boost::lexical_cast<unsigned short>(optarg);
+            break;
+        }
+    }
+
     struct nop_handler nh;
     struct cerr_status_listener mc;
     boost::shared_ptr<receiver_t> receiver(new receiver_t(nh, mc,
-                                                          "127.0.0.1", true));
-    receiver->join("239.1.2.3", 12345);
-    receiver->leave("239.1.2.3", 12345);
+                                                          opts_interface_ipaddr.to_string().c_str(),
+                                                          true));
+    receiver->join(opts_ipaddr.to_string().c_str(), opts_port);
     receiver->run();
+    receiver->leave(opts_ipaddr.to_string().c_str(), opts_port);
     return 0;
 }
